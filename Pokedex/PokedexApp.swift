929e4a2479e60004daca7e4929bcc7963928f123
isaacbmiller <isaacbmiller@gmail.com>
2022-11-14 20:50:05 +0100

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Isaac Miller on 11/14/22.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
